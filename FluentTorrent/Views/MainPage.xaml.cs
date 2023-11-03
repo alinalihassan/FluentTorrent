9d811e2cec7faae9085bcf587616e56a65de4442
Alin Ali Hassan <hassanalinali@gmail.com>
2023-11-03 22:53:17 +0100

    Added Pivot for Details Panel, added menu flyout to datagrid rows

--- FluentTorrent/Views/MainPage.xaml.cs
+++ FluentTorrent/Views/MainPage.xaml.cs
@@ -1,7 +1,9 @@
-﻿using FluentTorrent.ViewModels;
+﻿using CommunityToolkit.WinUI.UI.Controls;
+using FluentTorrent.ViewModels;
 using FluentTorrent.Views.Modals;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 using Windows.Storage;
 using Windows.Storage.Pickers;
 using WinRT.Interop;
@@ -21,6 +23,45 @@ public sealed partial class MainPage : Page
         InitializeComponent();
     }
 
+    #region Grid Events
+    private void Grid_PointerPressed(object sender, PointerRoutedEventArgs e)
+    {
+        TorrentGrid.SelectedItems.Clear();
+        TorrentGrid.SelectedItem = null;
+    }
+
+    private void TorrentGrid_RightTapped(object sender, RightTappedRoutedEventArgs e)
+    {
+        if (sender is DataGridRow row)
+        {
+            MenuFlyout flyout = new MenuFlyout();
+
+            // Make items
+            MenuFlyoutItem flyout_resume = new MenuFlyoutItem { Text = "Resume" };
+            MenuFlyoutItem flyout_pause = new MenuFlyoutItem { Text = "Pause" };
+            MenuFlyoutSeparator flyout_sep1 = new MenuFlyoutSeparator();
+            MenuFlyoutItem flyout_delete = new MenuFlyoutItem { Text = "Delete" };
+
+            // Add items
+            flyout.Items.Add(flyout_resume);
+            flyout.Items.Add(flyout_pause);
+            flyout.Items.Add(flyout_sep1);
+            flyout.Items.Add(flyout_delete);
+
+            // Show the MenuFlyout
+            flyout.ShowAt(row, e.GetPosition(row));
+        }
+    }
+
+    private void TorrentGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+    {
+        DataGridRow row = e.Row;
+
+        row.RightTapped += TorrentGrid_RightTapped;
+    }
+    #endregion
+
+    #region AppBar Buttons
     private async void AppBarButton_AddTorrent(object sender, RoutedEventArgs e)
     {
         var filePicker = new FileOpenPicker();
@@ -50,4 +91,5 @@ public sealed partial class MainPage : Page
 
         await dialog.ShowAsync();
     }
+    #endregion
 }
