57d99a979fb06a5594b32d775f2c5fcfe758366b
Alin Ali Hassan <hassanalinali@gmail.com>
2023-11-03 22:18:51 +0100

    Added initial Add and Remove button functionality

--- FluentTorrent/Views/MainPage.xaml.cs
+++ FluentTorrent/Views/MainPage.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using FluentTorrent.ViewModels;
-
+using FluentTorrent.Views.Modals;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using WinRT.Interop;
 
 namespace FluentTorrent.Views;
 
@@ -15,5 +19,35 @@ public sealed partial class MainPage : Page
     {
         ViewModel = App.GetService<MainViewModel>();
         InitializeComponent();
+    }
+
+    private async void AppBarButton_AddTorrent(object sender, RoutedEventArgs e)
+    {
+        var filePicker = new FileOpenPicker();
+        filePicker.FileTypeFilter.Add(".torrent");
+
+        InitializeWithWindow.Initialize(filePicker, WindowNative.GetWindowHandle(App.MainWindow));
+        StorageFile file = await filePicker.PickSingleFileAsync();
+
+        if (file != null)
+        {
+            // Do something with the file.
+        }
+    }
+
+    private async void AppBarButton_DeleteTorrent(object sender, RoutedEventArgs e)
+    {
+        ContentDialog dialog = new ContentDialog()
+        {
+            XamlRoot = XamlRoot,
+            Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style,
+            Title = "Remove Torrent",
+            PrimaryButtonText = "Delete",
+            CloseButtonText = "Cancel",
+            DefaultButton = ContentDialogButton.Primary,
+            Content = new DeleteDialogContent()
+        };
+
+        await dialog.ShowAsync();
     }
 }
---
+++ FluentTorrent/Views/Modals/DeleteDialogContent.xaml.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace FluentTorrent.Views.Modals;
+public sealed partial class DeleteDialogContent : Page
+{
+    public DeleteDialogContent()
+    {
+        this.InitializeComponent();
+    }
+}
